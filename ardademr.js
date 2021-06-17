1811a273ec73a809b27a421dab9e0fd59b1e397d
Glitch (hello-express) <none>
2021-06-17 00:18:58 +0000

    🐑🌓 Checkpoint ./OKU.MD:29130802/140 ./ardademr.js:29130802/2267 ./package.json:29130802/25 ./events/message.js:29130802/232 ./komutlar/destek.js:29130802/2029

--- ardademr.js
+++ ardademr.js
@@ -1,15 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const express = require("express");
-const {
-    JsonDatabase
-  } = require("wio.db");
-  const db = new JsonDatabase("myDatabase");
-  const db2 = new JsonDatabase("oto-kayit");
+const db = require('quick.db');
 const app = express();
 
 const fs = require("fs");
-                              // ArdaDemr Youtube Kanalına ait yapay zekalı kayıt botu altyapısı
+                              // ArdaDemr Youtube Kanalına ticket botu altyapısı
 //Uptime için__________________________________________________________________
 app.get("/", (req, res) => {
   res.send("ArdaDemr Discord Bot Altyapısı");
@@ -46,50 +42,15 @@ client.on("ready", () => {
 });
 
 client.login(process.env.TOKEN);
-// ArdaDemr Youtube Kanalına ait yapay zekalı kayıt botu altyapısı
+// ArdaDemr Youtube Kanalına ticket botu altyapısı
 
 
-//---------------------KAYIT SİSTEMİ-------------------------\\
-var isimler = require("./isimler");
-
-client.on("message",message=>{
-if(message.channel.type=="dm") return false;
-if(message.author.bot) return false;
-if(message.attachments.size > 0) return false;
-let isimkanali = db2.get(`isimkanali_${message.guild.id}`)
-const isimkanalimesaj = message.guild.channels.cache.find(kanal => kanal.id === isimkanali);
-if(isimkanali) {
-  if(message.channel.id == `${isimkanali}`){
-    if(!isimler.map(a=>a.name).includes(message.content.toLowerCase().split("")[0].toUpperCase()+message.content.toLowerCase().slice(1))){
-	message.reply("gerçek adın nedir?").then(message=>{
-setTimeout(function(){
-message.delete();
-},5000)
-
-});
-setTimeout(function(){
-message.delete();
-},5000)
-
-    }else{
-        let verilecekrol = db2.fetch(`isimrol_${message.guild.id}`)
-        let alinacakrol = db2.fetch(`isimrolal_${message.guild.id}`)
-        var tag = db2.get(`isimtag_${message.guild.id}`) || "";
-setTimeout(function(){
-  message.guild.member(message.author.id).setNickname(tag+" "+message.content.toLowerCase().split("")[0].toUpperCase()+message.content.toLowerCase().slice(1));
-},1500)
-setTimeout(function(){
-  if(db2.has(`isimrolal_${message.guild.id}`)) {
-    message.guild.member(message.author).roles.remove(alinacakrol);
-      }
-},2500)
-setTimeout(function(){
-  if(db2.has(`isimrol_${message.guild.id}`)) {
-    message.guild.member(message.author).roles.add(verilecekrol);
-      }
-},3500)
-       };
-     }
-  }
-});
-//---------------------KAYIT SİSTEMİ-------------------------\\
+//-----------------------DESTEK KAPATMA-----------------------\\
+client.on("message", message => {
+  if (message.content.toLowerCase() === "!destek-kapat") {
+  if (!message.channel.name.startsWith(`destek-`)) return
+   message.channel.delete()
+   db.delete(`destek_${message.author.id}`)
+        }
+      })
+//-----------------------DESTEK KAPATMA-----------------------\\
---
+++ komutlar/destek.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const db = require('quick.db');
+
+exports.run = async (client, message, args) => {
+	message.delete({ timeout: 5000 })
+    if(message.channel.id !== "DESTEK KANAL ID") return message.reply('bu komut sadece destek kanalında çalışır.').then(message => { message.delete({ timeout: 5000 })})
+   if (message.guild.channels.cache.get(db.fetch(`destek_${message.author.id}`))) return false || message.channel.send('Şu anda mevcut bir talebin var.').then(message => { message.delete({ timeout: 5000 })})
+      
+    let kullanici = message.author
+    let yetkili = message.guild.roles.cache.find(x => x.id == "YETKILI ROL ID") // YETKİLİ ROL İD
+    let herkes = message.guild.roles.cache.find(x => x.name == "@everyone")
+    message.guild.channels.create(`destek-${message.author.username}`, "text").then(async c => {
+        db.set(`destek_${message.author.id}`, c.id)
+        const category = message.guild.channels.cache.get('KATEGORİ ID') // Kategori id
+        c.setParent(category.id)
+        c.send(`${kullanici} destek talebin oluşturuldu. Destek ekibi seninle buradan ilgilenecek.\nTalebi kapatmak icin !destek-kapat`)
+        c.overwritePermissions([
+            {
+                id: kullanici.id,
+                allow: ['VIEW_CHANNEL','READ_MESSAGE_HISTORY','SEND_MESSAGES'],
+            },
+            {
+                id: yetkili.id,
+                allow: ['VIEW_CHANNEL','READ_MESSAGE_HISTORY','SEND_MESSAGES'],
+            },
+            {
+                id: herkes.id,
+                deny: ['VIEW_CHANNEL','READ_MESSAGE_HISTORY','SEND_MESSAGES'],
+            },
+        ]);
+    
+})
+
+
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'destek'
+};
