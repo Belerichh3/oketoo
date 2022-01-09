d5450b941267f04b3347a6e2ff72b910390b1526
Glitch (titanium-distinct-stork) <none>
2022-01-09 22:09:00 +0000

    🌖👨‍👨‍👦 Checkpoint ./package.json:25915949/82 ./komutlar/destek.js:25915949/95

--- komutlar/destek.js
+++ komutlar/destek.js
@@ -3,15 +3,15 @@ const db = require('quick.db');
 // ArdaDemr Youtube Kanalına ticket botu altyapısı
 exports.run = async (client, message, args) => {
 	message.delete({ timeout: 5000 })
-    if(message.channel.id !== "DESTEK KANALI ID") return message.reply('bu komut sadece destek kanalında çalışır.').then(message => { message.delete({ timeout: 5000 })})
+    if(message.channel.id !== "929858627618103356") return message.reply('bu komut sadece destek kanalında çalışır.').then(message => { message.delete({ timeout: 5000 })})
    if (message.guild.channels.cache.get(db.fetch(`destek_${message.author.id}`))) return false || message.channel.send('Şu anda mevcut bir talebin var.').then(message => { message.delete({ timeout: 5000 })})
       
     let kullanici = message.author
-    let yetkili = message.guild.roles.cache.find(x => x.id == "YETKILI ROL ID") // YETKİLİ ROL İD
+    let yetkili = message.guild.roles.cache.find(x => x.id == "812989489852973077") // YETKİLİ ROL İD
     let herkes = message.guild.roles.cache.find(x => x.name == "@everyone")
     message.guild.channels.create(`destek-${message.author.username}`, "text").then(async c => {
         db.set(`destek_${message.author.id}`, c.id)
-        const category = message.guild.channels.cache.get('KATEGORI ID') // Kategori id
+        const category = message.guild.channels.cache.get('927246842973020200') // Kategori id
         c.setParent(category.id)
         c.send(`${kullanici} destek talebin oluşturuldu. Destek ekibi seninle buradan ilgilenecek.\nTalebi kapatmak icin !destek-kapat`)
         c.overwritePermissions([
